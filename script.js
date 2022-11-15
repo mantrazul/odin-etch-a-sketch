6ebd719e216799a5882de79d984daa0b077c108a
mantrazul <aspirina495@gmail.com>
2022-11-15 20:01:25 -0300

    Add a prototype test grid

---
+++ script.js
@@ -0,0 +1,17 @@
+
+console.log('hello')
+
+
+
+const mainGrid = document.querySelector('#grid');
+
+for (let i = 0; i < 16; i++ ) {
+const content = document.createElement('div');
+content.classList.add('test')
+mainGrid.appendChild(content);
+}
+
+// content.innerHTML = ''
+
+mainGrid.appendChild(content);
+
