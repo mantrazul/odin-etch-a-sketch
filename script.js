8625b3d899deb8727073e1c68d96590b6aa5597d
mantrazul <aspirina495@gmail.com>
2022-11-15 21:10:47 -0300

    Introduce first working prototype of the Etch-a-Sketch

--- script.js
+++ script.js
@@ -5,13 +5,24 @@ console.log('hello')
 
 const mainGrid = document.querySelector('#grid');
 
+
 for (let i = 0; i < 16; i++ ) {
 const content = document.createElement('div');
 content.classList.add('test')
 mainGrid.appendChild(content);
 }
 
+
 // content.innerHTML = ''
 
-mainGrid.appendChild(content);
+// const example = document.querySelector('div.test'); 
 
+    
+        const gridItems = document.querySelectorAll("#grid > div")
+
+        gridItems.forEach((items => {
+            const gridItem = items;
+            gridItem.addEventListener("click", () => {
+                gridItem.classList.add("test2")
+                } );
+        }))
