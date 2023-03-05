c3be1acd843ef7d66d4fca1bdd7a7d90f3ed9f07
mantrazul <aspirina495@gmail.com>
2023-03-05 18:00:51 -0300

    update: start over with help from other examples

--- script.js
+++ script.js
@@ -1,53 +1,57 @@
+const DEFAULT_COLOR = '#33333';
+const DEFAULT_SIZE = 16;
+const DEFAULT_MODE = 'paint';
 
-console.log('hello')
+let currentColor = DEFAULT_COLOR;
+let currentSize = DEFAULT_SIZE;
+let currentMode = DEFAULT_MODE;
+
+const colorPicker = document.getElementById("colorpicker")
+const testBox = document.getElementsByClassName("test-box")
+const checkBox = document.querySelector(".check")
+const grid = document.querySelector(".grid")
 
 
-
-const mainGrid = document.querySelector('#grid');
-
-
-for (let i = 0; i < 16; i++ ) {
-const content = document.createElement('div');
-content.classList.add('test')
-mainGrid.appendChild(content);
+function updateColor(newColor) {
+    currentColor = newColor
 }
+
+function checkColor() {
+    console.log(currentColor)
+    
+}
+
+function bgChange(e) {
+    e.target.style.backgroundColor = currentColor;
+    console.log(e);
+  }
+
+  function setupGrid(size) {
+    grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`
+    grid.style.gridTemplateRows = `repeat(${size}, 1fr)`
   
-
-        const gridItems = document.querySelectorAll("#grid > div")
-
-        const paintButton = document.querySelector(".paint");
-
-        paintButton.addEventListener("click", () => {
-            paint()
-        })
-
-
-    function paint() {
-
-            gridItems.forEach((items => {
-                const gridItem = items;
-                gridItem.addEventListener("click", () => {
-                    gridItem.classList.add("test2")
-                    } );
-                    console.log("paint")
-        }))
+    for (let i = 0; i < size * size; i++) {
+      const gridElement = document.createElement('div')
+      gridElement.classList.add('grid-element')
+      gridElement.addEventListener('mouseover', changeColor)
+      gridElement.addEventListener('mousedown', changeColor)
+      grid.appendChild(gridElement)
     }
+  }
 
- 
-    const eraseButton = document.querySelector(".erase");
+  function changeColor(e) {
+    if (e.type === 'mouseover' && !mouseDown) return
+    e.target.style.backgroundColor = currentColor
+  }
 
-    eraseButton.addEventListener("click", () => {
-        erase()
-    })
+let mouseDown = false
+document.body.onmousedown = () => (mouseDown = true)
+document.body.onmouseup = () => (mouseDown = false)
 
+checkBox.addEventListener("click", checkColor)
+checkBox.addEventListener("click", bgChange)
+colorPicker.oninput = (event) => updateColor(event.target.value);
 
-    function erase() {
-
-        gridItems.forEach((items => {
-            const gridItem = items;
-            gridItem.addEventListener("click", () => {
-                gridItem.classList.remove("test2")
-                } );
-                console.log("erase")
-    }))
-    }
+window.onload = () => {
+    setupGrid(DEFAULT_SIZE)
+  }
